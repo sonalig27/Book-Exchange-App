87f552287ca74e5ce2ff93d40a5168b4fefead9b
Sonali Ganatra <49038910+sonalig27@users.noreply.github.com>
2019-11-17 23:26:32 -0500

    Project front end startup code
    Project front end startup code

---
+++ login.js
@@ -0,0 +1,63 @@
+/* <script type="text/javascript">
+document.getElementById("myButton").onclick = function () {
+    location.href = "file:///C:/Users/ketul/OneDrive/Desktop/Sem2/IWS/Project/ISBN.html";
+};
+</script> */
+
+
+function validateEmail(emailField){
+    var reg = /^[a-zA-Z0-9_]+@student\.uml\.edu$/;
+
+    if (reg.test(emailField.value) == false) 
+    {
+        alert('Enter a valid UML student email address');
+        return false;
+    }
+
+    return true;
+
+}
+
+
+// Below Function Executes On Form Submit
+function loadPage() {
+    // Storing Field Values In Variables
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    // Regular Expression For Email
+    var emailReg = /^[a-zA-Z0-9_]+@student\.uml\.edu$/;
+    // Conditions
+    if(email == ''){
+        alert("Email can't be empty");
+        return false;
+    }
+    if (!email.match(emailReg)) {
+        alert('Enter a valid UML student email address');
+        return false;
+    }
+    if (password == '') {
+        alert("Password can't be empty");
+        return false;
+    }
+    if(password.length < 5){
+        alert("Password you entered is incorrect");
+        return false;
+        }
+    return true;
+    }
+
+    function yesnoCheck() {
+        if (document.getElementById('Sell').checked) {
+             document.getElementById('rentDue').style.display = 'none';
+            // document.getElementById('rentDeposit').style.display = 'none';
+         document.getElementById('rentDueLabel').style.display = 'none';
+            // document.getElementById('rentDepositLabel').style.display = 'none';
+            document.getElementById("rentDepositAmount").style.display = 'none';
+        }else {
+             document.getElementById('rentDue').style.display = 'block';
+            // document.getElementById('rentDeposit').style.display = 'block';
+             document.getElementById('rentDueLabel').style.display = 'block';
+            // document.getElementById('rentDepositLabel').style.display = 'block';
+            document.getElementById("rentDepositAmount").style.display = 'block';
+        }    
+    }
